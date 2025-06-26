d292a412ebfc67f82ee4e54e1eb170118151a807
SpiderMan <hrushikesh.tk@gmail.com>
2025-06-26 22:47:05 +0530

    Callables and FixedThreadPool

--- src/concurrency02/FibonacciUsingFixedThreadPool.java
+++ src/concurrency02/FibonacciUsingFixedThreadPool.java
@@ -1,18 +1,51 @@
 package concurrency02;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class FibonacciUsingFixedThreadPool implements Callable<Integer> {
 
-    public int fib(int num) {
-        if (num == 0 || num == 1) {
-            return num;
-        }
-        return fib(num-1) + fib(num-2);
+    private int n;
+
+    public FibonacciUsingFixedThreadPool(int n) {
+        this.n = n;
     }
+
+//    public int fib(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//        return fib(n-1) + fib(n-2);
+//    }
 
     @Override
     public Integer call() throws Exception {
-        return 0;
+        System.out.println(n + " : from Thread" + Thread.currentThread().getName());
+        if (n <= 1) {
+            return n;
+        }
+        // Creating a FixedThreadPool:
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        //fib(n-1) + fib(n-2); :
+        Future<Integer> futureAns1 =  executorService.submit(new FibonacciUsingFixedThreadPool(n-1));
+        Future<Integer> futureAns2 =executorService.submit(new FibonacciUsingFixedThreadPool(n-2));
+        // execute has void return type, so we have to use "submit" over here to catch some result.
+        // futureAns1 and futureAns2 are like receipts, which are waiting for the output.
+        //  --> FUTURE interface in Java primarily to represent the result of an asynchronous computation.
+        //  It acts as a handle or a placeholder for a value that may not be available yet,
+        //  but will be at some point in the future.
+        // Future is like a receipt kind of thing during a blood test.
+
+        int ans1 = futureAns1.get();
+        int ans2 = futureAns2.get();
+        // Here we are giving the receipt and taking the reports/answer.
+
+        return ans1 + ans2;
     }
+    // Here in the above scenario, we ended up creating Multiple Thread Pools,
+    // each thread pool containing 2 threads.
+    // So, In scenarios where we will not able to figure out, how many threads we need,
+    // We can make use of the CachedThreadPool.
 }
--- src/concurrency02/FibonacciUsingFixedThreadPoolMainClient.java
+++ src/concurrency02/FibonacciUsingFixedThreadPoolMainClient.java
@@ -1,11 +1,25 @@
 package concurrency02;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class FibonacciUsingFixedThreadPoolMainClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         // Normal Version ::
         FibonacciNumber fibonacciNum = new FibonacciNumber();
         int answer = fibonacciNum.fib(9);
-        System.out.println("answer is :: " + answer);
+//        System.out.println("answer is :: " + answer);
+
+        // FibonacciUsingFixedThreadPool ::
+
+        // Here I want to make the root call/ top-most call i.e fib(n),
+        //So, instead of engaging MAIN thread, I am creating a new thread and using it.
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        // We have to use "submit", if we have to catch some value.
+        Future<Integer> futureAns = executorService.submit(new FibonacciUsingFixedThreadPool(6));
+        System.out.println(futureAns.get());
     }
 }
